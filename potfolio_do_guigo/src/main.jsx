b9eb9b4a5b7b1df2a7d43e2f48bfa2dfc2aac637
Guilherme Ancheschi Werneck Pereira <guilhermewerneckpereira11@hotmail.com>
2025-11-25 10:49:49 -0300

    Adição do Footer e ScrollToTopButton

--- potfolio_do_guigo/src/main.jsx
+++ potfolio_do_guigo/src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import Footer from './components/Footer.jsx'
+import ScrollToTopButton from './components/ScrollToTopButtons.jsx'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Footer />
+    <ScrollToTopButton />
   </StrictMode>,
 )
